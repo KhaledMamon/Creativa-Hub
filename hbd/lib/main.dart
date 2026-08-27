49696ae47d17a8dc7aee0873fb5a93f430d90b63
KhaledMamon <khaledgamal.boy@gmail.com>
2026-08-27 16:22:46 +0300

    first Task in flutter

---
+++ hbd/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Spacer(),
+            Text(
+              'HBD \nKhaled',
+              style: TextStyle(fontSize: 30, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            Spacer(),
+            Image.asset(
+              'images/1.png',
+              width: 500,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
